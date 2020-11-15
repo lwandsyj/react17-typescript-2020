d1c6fdff4ac0f4854182b48ed65f635cd205a3ee
li <lwaisyj@163.com>
2020-11-15 21:27:36 +0800

    ts

--- code/ts-learn/app.ts
+++ code/ts-learn/app.ts
@@ -1,3 +1,16 @@
-let a:number =null;
-a=3;
-console.log(a);
+interface People{
+    name:string,
+    get_name():string;
+    get_name2:()=>string;
+}
+
+let a :People ={
+    name:"zhangsan",
+    get_name():string{
+        return this.name
+    },
+    get_name2:()=>{
+        return this.name
+        
+    }
+}
