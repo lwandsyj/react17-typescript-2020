70b92365fde1dbd0a38591cd1f7c4970609be19c
li <lwaisyj@163.com>
2020-11-17 16:27:52 +0800

    静态资源

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,15 +1,33 @@
-import  Koa,{Context} from 'koa'
+import Koa from 'koa'
 import router from './router'
-import {validateJwt} from './middelware'
-import { nextTick } from 'process';
-const app:Koa =new Koa();
+import path from 'path'
+import koaBody from 'koa-body';
+import StaticCache from 'koa-static-cache'
 
-app.use(validateJwt("test"));
-app.use(async (ctx,next)=>{
-    console.log(ctx.state)
-    await next()
-})
+const app = new Koa();
+
+app.use(StaticCache({
+    dir: path.join(__dirname, '../static'),
+    prefix: '/static'
+}));
+
+app.use(koaBody({
+    multipart: true,
+    formidable: {
+        uploadDir: path.join(__dirname, '../public/upload')
+    }
+}));
+
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.listen(4000)
+// 处理路由写错地址或者请求方式，或者不存在的路由
+app.use(async (ctx) => {
+    ctx.body = {
+        code: 1004,
+        msg: '路由不存在'
+    }
+});
+app.listen(4000, () => {
+    console.log('start at 4000')
+});
