de54fd1f2ba1d6bd900d12e4665cbf863ed19d48
li <lwaisyj@163.com>
2021-01-20 16:01:09 +0800

    ts

--- code/pdf/pdf.js
+++ code/pdf/pdf.js
@@ -5,6 +5,7 @@ const filePath = path.join(__dirname,'./1.pdf')
 const buffer =fs.readFileSync(filePath)
 
 pdf(buffer).then((data)=>{
+    
     const str="<html><body>"+data.text+"</body></html>"
     fs.writeFile(path.join(__dirname,'./index1.html'),str,(err)=>{
         console.log('success')
--- code/pdf/server.js
+++ code/pdf/server.js
@@ -2,7 +2,7 @@ let Epub = require('epub')
 const { fstat } = require('fs')
 const path=require('path')
 const fs =require('fs')
-const filePath = path.join(__dirname,'./1.epub')
+const filePath = path.join(__dirname,'./2.epub')
 const app=new Epub(filePath)
 let arr=[]
 app.on('end',()=>{
@@ -13,18 +13,10 @@ app.on('end',()=>{
     app.flow.forEach((item,index)=>{
         app.getChapter(item.id,(err,txt)=>{
             i++;
-            console.log(app.manifest)
-            app.getImage(item.id,(err,data,minetype)=>{
-                if(err){
-                    console.log('err',err)
-                    return
-                }
-                console.log(data)
-            })
            arr.push(txt)
            if(i==app.flow.length){
             const str="<html><body>"+arr.join()+"</body></html>"
-            fs.writeFile(path.join(__dirname,'./index.html'),str,(err)=>{
+            fs.writeFile(path.join(__dirname,'./index-1.html'),str,(err)=>{
         
             })
            }
